5c6321f6605749485871cd24e7077f1fa28d5579
xaliphostes <fmaerten@gmail.com>
2018-06-27 11:45:19 +0200

    moving file

--- Main/src/main.cxx
+++ Main/src/main.cxx
@@ -1,8 +1,7 @@
 #include "Hello.h"
 
-int main(int argc, char *argv[])
-{
-    Hello hi;
-    hi.print();
-    return 0;
+int main(int argc, char *argv[]) {
+    Hello hi ;
+    hi.print() ;
+    return 0 ;
 }
